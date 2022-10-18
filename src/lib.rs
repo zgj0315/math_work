0f960daf4d7fbb8e1b87ddb2220dc617cf6bbd06
赵光建 <zhaoguangjian@cyberkl.com>
2022-10-18 09:32:20 +0800

    impl

--- src/lib.rs
+++ src/lib.rs
@@ -12,61 +12,76 @@ pub enum Symbol {
     Divide,
 }
 
+#[derive(Debug)]
 pub struct Formula {
     pub x: i32,
     pub y: i32,
     pub z: i32,
 }
 
-pub fn make_formula(symbol: Symbol, _difficulty: &Difficulty) -> Formula {
-    let mut rng = thread_rng();
-    let mut x: i32;
-    let mut y: i32;
-    loop {
-        x = rng.gen_range(10..99);
-        y = rng.gen_range(10..99);
-        let x_tens = x / 10;
-        let x_ones = x % 10;
-        let y_tens = y / 10;
-        let y_ones = y % 10;
-        match _difficulty {
-            Difficulty::Easy => match symbol {
-                Symbol::Add | Symbol::Subtract => {
-                    if (x_ones + y_ones) < 10 && (x_tens + y_tens) < 10 {
-                        break;
+impl Formula {
+    pub fn new(symbol: Symbol, difficulty: &Difficulty) -> Formula {
+        let mut rng = thread_rng();
+        let mut x: i32;
+        let mut y: i32;
+        loop {
+            x = rng.gen_range(10..99);
+            y = rng.gen_range(10..99);
+            let x_tens = x / 10;
+            let x_ones = x % 10;
+            let y_tens = y / 10;
+            let y_ones = y % 10;
+            match difficulty {
+                Difficulty::Easy => match symbol {
+                    Symbol::Add | Symbol::Subtract => {
+                        if (x_ones + y_ones) < 10 && (x_tens + y_tens) < 10 {
+                            break;
+                        }
                     }
-                }
-                Symbol::Multiply | Symbol::Divide => {
-                    if (x_ones * y_ones) < 10 && (x_tens * y_tens) < 10 {
-                        break;
+                    Symbol::Multiply | Symbol::Divide => {
+                        if (x_ones * y_ones) < 10 && (x_tens * y_tens) < 10 {
+                            break;
+                        }
                     }
+                },
+                Difficulty::Normal => {
+                    break;
                 }
-            },
-            Difficulty::Normal => {
-                break;
+                Difficulty::Hard => match symbol {
+                    Symbol::Add | Symbol::Subtract => {
+                        if (x_ones + y_ones) >= 10 && (x_tens + y_tens) >= 10 {
+                            break;
+                        }
+                    }
+                    Symbol::Multiply | Symbol::Divide => {
+                        if (x_ones * y_ones) >= 10 && (x_tens * y_tens) >= 10 {
+                            break;
+                        }
+                    }
+                },
             }
-            Difficulty::Hard => match symbol {
-                Symbol::Add | Symbol::Subtract => {
-                    if (x_ones + y_ones) >= 10 && (x_tens + y_tens) >= 10 {
-                        break;
-                    }
-                }
-                Symbol::Multiply | Symbol::Divide => {
-                    if (x_ones * y_ones) >= 10 && (x_tens * y_tens) >= 10 {
-                        break;
-                    }
-                }
-            },
+        }
+        match symbol {
+            Symbol::Add | Symbol::Subtract => {
+                let z = x + y;
+                Formula { x, y, z }
+            }
+            Symbol::Multiply | Symbol::Divide => {
+                let z = x * y;
+                Formula { x, y, z }
+            }
         }
     }
-    match symbol {
-        Symbol::Add | Symbol::Subtract => {
-            let z = x + y;
-            Formula { x, y, z }
-        }
-        Symbol::Multiply | Symbol::Divide => {
-            let z = x * y;
-            Formula { x, y, z }
-        }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        let symbol = Symbol::Add;
+        let difficulty = Difficulty::Normal;
+        let formual = Formula::new(symbol, &difficulty);
+        println!("formual: {:?}", formual);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use chrono::prelude::*;
-use math_work::{make_formula, Difficulty, Symbol};
+use math_work::{Difficulty, Formula, Symbol};
 use std::{env, fs, io::Write, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut _difficulty = Difficulty::Normal;
+    let mut difficulty = Difficulty::Normal;
     if args.len() == 2 {
         if &args[1] == "easy" {
-            _difficulty = Difficulty::Easy;
+            difficulty = Difficulty::Easy;
         } else if &args[1] == "hard" {
-            _difficulty = Difficulty::Hard;
+            difficulty = Difficulty::Hard;
         } else {
             println!("args error, please input {} [easy|hard]", args[0]);
             process::exit(1);
@@ -33,10 +33,10 @@ fn main() {
         fs::File::create(file_path_formula_result).expect("create failed");
 
     for _n in 1..14 {
-        let add = make_formula(Symbol::Add, &_difficulty);
-        let subtract = make_formula(Symbol::Subtract, &_difficulty);
-        let multiply = make_formula(Symbol::Multiply, &_difficulty);
-        let divide = make_formula(Symbol::Divide, &_difficulty);
+        let add = Formula::new(Symbol::Add, &difficulty);
+        let subtract = Formula::new(Symbol::Subtract, &difficulty);
+        let multiply = Formula::new(Symbol::Multiply, &difficulty);
+        let divide = Formula::new(Symbol::Divide, &difficulty);
         let line = format!(
             "{} + {} =,{} - {} =,{} * {} =,{} / {} =\n",
             add.x, add.y, subtract.z, subtract.x, multiply.x, multiply.y, divide.z, divide.x,
